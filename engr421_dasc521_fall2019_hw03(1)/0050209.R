037a63f754159c424e544a8f70e70e3819788f42
Emre <uludag.emre.tr@gmail.com>
2019-10-31 14:19:59 +0300

    Still work on dimensionality issues

--- engr421_dasc521_fall2019_hw03(1)/0050209.R
+++ engr421_dasc521_fall2019_hw03(1)/0050209.R
@@ -1,4 +1,4 @@
-setwd('/home/emre/Documents/CollegeFiles/COMP421/MachineLearningCourse/engr421_dasc521_fall2019_hw03(1)/')
+setwd('/Users/euludag14/Documents/MachineLearningCourse/engr421_dasc521_fall2019_hw03(1)/')
 
 images_data <- read.csv("hw03_images.csv",header=FALSE)
 labels_data <- read.csv("hw03_labels.csv",header=FALSE)
@@ -9,6 +9,9 @@ initial_W_data <- read.csv("initial_W.csv",header=FALSE)
 y_truth <- labels_data$V1
 y_truth_training_set <- head(y_truth,n=500)
 y_truth_test_set <- tail(y_truth,n=500)
+
+y_truth_training_matrix <- data.matrix(y_truth_training_set)
+y_truth_test_matrix <- data.matrix(y_truth_test_set)
 
 x_training <- data.matrix(head(images_data,n=500))
 x_test <- data.matrix(tail(images_data,n=500))
@@ -22,24 +25,32 @@ D <- ncol(images_data)
 #gradient function of v
 v_gradient <- function(y_truth,y_predicted,x_data){
   minus_transpose <- -t(x_data)
-  bias<-(y_truth - y_predicted)
+  bias<-(y_truth - rowSums(y_predicted)/ncol(y_predicted))
   return( minus_transpose %*% bias )
 }
 #gradient function of w
 
 w_gradient <- function(y_truth, y_predicted,X, Z, V){
-  number_of_oberservations <- nrow(V)
-  bias<-(y_truth - y_predicted)
-  transpose_V <- t(V)
-  minus_transpose_X <- -t(X)
-  output_not_cropped <- (minus_transpose_X %*% ((bias %*% transpose_V) * 1-Z * Z))
-  output <- output_not_cropped[,2:number_of_oberservations]
-  return (output)
+  number_of_obs <- nrow(V)
+  bias<-(y_truth - rowSums(y_predicted)/ncol(y_predicted))
+  print(dim(y_truth))
+  print(dim(V))
+  bias <- matrix(  rep(bias ,ncol(y_truth)),nrow=nrow(y_truth), ncol=ncol(y_truth)   )
+  print(dim(bias))
+  # transpose_V <- t(V)
+  #  minus_transpose_X <- -t(X)
+  #output_not_cropped <- (minus_transpose_X %*% ((bias %*% transpose_V) * 1-Z * Z))
+  #output <- output_not_cropped[,2:number_of_obs]
+  return 
 }
 
 #the sigmoid function
 sigmoid <- function(a) {
   return (1 / (1 + exp(-a)))
+}
+#the safelog function
+safelog <- function(a){
+  return ( log( a + 1e-100))
 }
 
 #the softmax function
@@ -54,7 +65,7 @@ softmax <- function(matrix){
   matrix_row_sums_sequence <- rep(matrix_row_sums, number_of_features)
   
   #Then I turned it into a matrix
-  matrix_row_sums_matrix <- matrix(matrix_row_sums_sequence, nrow = n, ncol = number_of_features)
+  matrix_row_sums_matrix <- matrix(matrix_row_sums_sequence, nrow = number_of_obs, ncol = number_of_features)
   
   return(matrix/matrix_row_sums_matrix)
 }
@@ -70,13 +81,42 @@ V <- data.matrix(initial_V_data)
 errors <- c()
 iteration_count <- 1
 
-while(1){
-    
+calculate_error <- function(y_truth,y_prediction){
+  return (-sum(t(y_truth) %*% safelog(y_prediction)))
 }
+#while(1){
+  
+  matrix_product <- sigmoid(cbind(1,x_training) %*% W)
+  y_training_predicted <- softmax(cbind(1,matrix_product) %*% V)
+  #dim(x_training) 500 784 thus we must bind the output layer 1's as coloum
+  #dim(W) 785 20
+  
+  prediction_error<- calculate_error(y_truth_training_matrix,y_training_predicted)
+  errors <- c(errors,prediction_error)
+  
+  delta_v <- eta * v_gradient(y_truth_training_matrix,y_training_predicted,cbind(1,matrix_product))
+  temp_delta_v <- matrix(rep(delta_v,ncol(V)),nrow=nrow(V),ncol=ncol(V))
+  V <- V - temp_delta_v
+  
+  delta_w <- eta * w_gradient(y_truth_training_matrix,y_training_predicted,cbind(1, x_training),matrix_product,V)
+  W <- W - delta_w
+
+  error_margin <- 1e+10
+#  if(iteration_count != 1){
+    error_margin<-abs(errors[iteration_count]-errors[iteration_count-1])
+ # }
+  if(error_margin < epsilon | iteration_count = max_iteration){
+    break
+  }
+  iteration_count<-iteration_count+1
+#}
+
+plot(1:iteration_count, errors,
+     type = "l", lwd = 2, las = 1,
+     xlab = "Iteration", ylab = "Error")
 
 
-#dim(x_training) 500 784 thus we must bind the output layer 1's as coloum
-#dim(W) 785 20
+
 
 
 
