d586db5aebb66323862c0f4d6fb2d4d821dbd6dd
Emre <uludag.emre.tr@gmail.com>
2019-11-10 18:21:55 +0300

    added hw4

--- engr421_dasc521_fall2019_hw04/50209_hw4.R
+++ engr421_dasc521_fall2019_hw04/50209_hw4.R
@@ -10,7 +10,7 @@ test_data <- data[151:272,]
 #parameters for regressogram
 bin_width <- 0.37
 origin <- 1.5
-x_maximum <- 5
+x_maximum <- max(data$eruptions)
 
 regressogram <- function(data, bin_width, origin, x_max){
   bin_number <- ceiling((data$eruptions - origin) / bin_width)
@@ -31,18 +31,8 @@ regressogram <- function(data, bin_width, origin, x_max){
 training_regressogram<-regressogram(training_data, bin_width,origin,x_maximum)
 
 plot(x = training_data$eruptions,
-     y = training_data$waiting,
-     xlab = 'Waiting time to next eruption (min)',
-     ylab = 'Eruption time (min)',
-     type = 'p',
-     pch = 20,
-     col = 'blue',
-     xlim = c(origin, x_maximum))
-points(x = test_data$eruptions,
-       y = test_data$waiting,
-       type = 'p',
-       pch = 20,
-       col = 'red')
+     y = training_data$waiting,xlab = 'Waiting time to next eruption (min)',ylab = 'Eruption time (min)',type = 'p',pch = 20,col = 'blue',xlim = c(origin, x_maximum))
+points(x = test_data$eruptions,y = test_data$waiting,type = 'p',pch = 20,col = 'red')
 
 dummy_x <- (1:nrow(training_regressogram) / (1/bin_width)) + origin
 draw_data <- data.frame(cbind(dummy_x,training_regressogram$bin_mean))
@@ -67,33 +57,20 @@ legend(x = min(data$eruptions)-0.2, y= max(data$waiting)-10 , legend = c('traini
 #w function for running mean smoother
 w <- function(x){
   x <- data.frame(abs(x))
-  x <- apply(X = x, 
-             MARGIN = c(1, 2), 
-             FUN = function(e){
-               if (e <= 0.5){
-                 e <- 1
-               }else{
-                 e <- 0
-               }
-             })
-  return (x)
+  x <- apply(X = x, MARGIN = c(1, 2), FUN = function(e){
+               if (e <= 0.5){e <- 1}
+               else{e <- 0}})return (x)
 }
 
 #running mean smoother function with data_interval, data and bin_width parameters
 rms <- function(data_interval, data, bin_width){
-  g_head <- sapply(X = data_interval, 
-                   FUN = function(x){
-                     sum(w((x - data$eruptions) / bin_width) * data$waiting) /
-                       sum(w((x - data$eruptions) / bin_width))
-                   })
+  g_head <- sapply(X = data_interval, FUN = function(x){sum(w((x - data$eruptions) / bin_width) * data$waiting) /sum(w((x - data$eruptions) / bin_width))})
   result <- data.frame(data_interval)
   result <- cbind(result, g_head)
   colnames(result) <- c('x', 'rms')
   return (result)
 }
-
 #Calculation of RMSE 
-
 bin_number <- ceiling((test_data$eruptions - origin) / bin_width)
 test_regressogram <- cbind(test_data, bin_number)
 test_regressogram <- merge(x = test_regressogram,y = training_regressogram,by = 'bin_number')
@@ -112,31 +89,46 @@ train_data_rms <- rms(data_interval = rms_data_interval,
                       bin_width = rms_bin_width)
 
 #plotting train_data, test_data and running mean smoother of train_data
-plot(x = training_data$eruptions,
-     y = training_data$waiting,
-     xlab = 'eruptions',
-     ylab = 'waiting',
-     type = 'p',
-     pch = 20,
-     col = 'blue',
-     xlim = c(rms_origin, rms_x_max))
-points(x = test_data$eruptions,
-       y = test_data$waiting,
-       type = 'p',
-       pch = 20,
-       col = 'red')
-lines(x = train_data_rms$x,
-      y = train_data_rms$rms,
-      xlab = 'eruptions',
-      ylab = 'waiting',
-      type = 'l')
+plot(x = training_data$eruptions,y = training_data$waiting,xlab = 'eruptions',ylab = 'waiting',type = 'p',pch = 20,col = 'blue',xlim = c(rms_origin, rms_x_max))
+points(x = test_data$eruptions,y = test_data$waiting,type = 'p',pch = 20,col = 'red')
+lines(x = train_data_rms$x,y = train_data_rms$rms,xlab = 'eruptions',ylab = 'waiting',type = 'l')
 title(main = 'h = 0.37')
-legend(x = min(data$eruptions)-0.2, y= max(data$waiting)-10, legend = c('training', 'test'),
-       col = c('blue', 'red'), pch = 20, xjust = 0, yjust = 0)
+legend(x = min(data$eruptions)-0.2, y= max(data$waiting)-10, legend = c('training', 'test'),col = c('blue', 'red'), pch = 20, xjust = 0, yjust = 0)
 
 #calculating RMSE of running mean smoother for test_data
 
-test_data_rms <- train_data_rms[test_data$eruptions * ((length(rms_data_interval) - 1) / (rms_x_max - rms_origin)) + 1, ]
-
+test_data_rms <- train_data_rms[ceiling(test_data$eruptions * ((length(rms_data_interval) - 1) / (rms_x_max - rms_origin+2)) + 1), ]
+test_data_rms_rmse <- sqrt(sum((test_data$waiting - test_data_rms$rms)**2) /nrow(test_data_rms))
 cat('Running Mean Smoother => RMSE is', test_data_rms_rmse,'when h is', rms_bin_width)
 
+#Kernel smoothing part
+#k function for kernel smoother
+k <- function(x){return ((1 / sqrt(2 * pi)) * exp(-(x ** 2) / 2))}
+#kernel smoother function with data_interval, data and bin_width parameters
+ks <- function(data_interval, data, bin_width){
+  g_head <- sapply(X = data_interval, FUN = function(x){sum(k((x - data$eruptions) / bin_width) * data$waiting) /sum(k((x - data$eruptions) / bin_width))})
+  result <- data.frame(data_interval)
+  result <- cbind(result, g_head)
+  colnames(result) <- c('x', 'ks')
+  return (result)
+}
+#kernel smoother parameters
+ks_bin_width <- 0.37
+ks_origin <- 1.5
+ks_x_max <- max(data$eruptions)
+ks_data_interval <- seq(from = ks_origin, to = ks_x_max, by = 0.1)
+
+#kernel smoother of train_data with bin width: 1
+train_data_ks <- ks(data_interval = ks_data_interval,data = training_data,bin_width = ks_bin_width)
+
+#plotting train_data, test_data and kernel smoother of train_data
+plot(x = training_data$eruptions,y = training_data$waiting,xlab = 'erruptions',ylab = 'waiting',type = 'p',pch = 20,col = 'blue',xlim = c(ks_origin, ks_x_max))
+points(x = test_data$eruptions,y = test_data$waiting,type = 'p',pch = 20,col = 'red')
+lines(x = train_data_ks$x,y = train_data_ks$ks,xlab = 'x',ylab = 'y',type = 'l')
+title(main = 'h = 0.37')
+legend(x = min(data$eruptions)-0.2, y= max(data$waiting)-10, legend = c('training', 'test'),col = c('blue', 'red'), pch = 20, xjust = 0, yjust = 0)
+
+#calculating RMSE of kernel smoother for test_data
+test_data_ks <- train_data_ks[ceiling(test_data$eruptions * ((length(ks_data_interval) - 1) / (ks_x_max - ks_origin+2))), ]
+test_data_ks_rmse <- sqrt(sum((test_data$waiting - test_data_ks$ks)**2) /nrow(test_data_ks))
+cat('Kernel Smoother => RMSE is', test_data_ks_rmse,'when h is', ks_bin_width)
